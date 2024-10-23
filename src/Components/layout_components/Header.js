b654981c77f4a03be014ee1a78f75762379762b1
Grawo1 <dafmen25372@gmail.com>
2024-10-23 18:10:12 +0200

    ZALOZENI CONSTANTS, pridani loga

--- src/Components/layout_components/Header.js
+++ src/Components/layout_components/Header.js
@@ -1,36 +1,51 @@
 import Link from 'next/link';
 import '@/styles/components/layout_components/header.css'; // Import global CSS
-
+import Constants from '../../Constants';
+import Image from 'next/image';
 const Header = () => {
     return (
         <header className="header">
             <section className='section'>
                 <div className="header__wrapper container">
                     <div className="header__logo">
+                        <div className="header__img-wrapper">
+                            <Image
+                                width={50}
+                                height={50}
+                                src="/main/logo.png"
+                                alt="Muzeum Chalupeni Logo"
+                                layout="responsive"
+                            />
+                        </div>
                         <Link href="/">
-                            <p>MyApartment</p>
+                            <p>{Constants.textBlocks.web_title}</p>
                         </Link>
                     </div>
                     <nav className="header__menu">
                         <ul className="header__nav">
                             <li className="header__nav-item">
-                                <Link href="/">
-                                    <p className="header__nav-link">Home</p>
-                                </Link>
-                            </li>
-                            <li className="header__nav-item">
                                 <Link href="/about">
-                                    <p className="header__nav-link">About Us</p>
+                                    <p className="header__nav-link">O nás</p>
                                 </Link>
                             </li>
                             <li className="header__nav-item">
                                 <Link href="/services">
-                                    <p className="header__nav-link">Services</p>
+                                    <p className="header__nav-link">Prohlídky</p>
+                                </Link>
+                            </li>
+                            <li className="header__nav-item">
+                                <Link href="/services">
+                                    <p className="header__nav-link">Exponáty</p>
+                                </Link>
+                            </li>
+                            <li className="header__nav-item">
+                                <Link href="/services">
+                                    <p className="header__nav-link">Fotogalerie</p>
                                 </Link>
                             </li>
                             <li className="header__nav-item">
                                 <Link href="/contact">
-                                    <p className="header__nav-link">Contact Us</p>
+                                    <p className="header__nav-link">Kontakty a rezervace</p>
                                 </Link>
                             </li>
                         </ul>
---
+++ src/Constants.js
@@ -0,0 +1,11 @@
+const Constants = {
+    textBlocks: {
+        web_title: "Muzeum Chalupění",
+    },
+    contacts: {
+        web_phone: "123 456 789",
+        web_email: "muzeum@chalupeni.cz"
+    }
+}
+
+export default Constants;
