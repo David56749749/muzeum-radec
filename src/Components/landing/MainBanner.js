4e5b922fd5d27036189320035de10b046a52dcef
David56749749 <dafmen25372@gmail.com>
2024-10-24 10:54:02 +0200

    main banner responsivenes

--- src/Components/landing/MainBanner.js
+++ src/Components/landing/MainBanner.js
@@ -1,31 +1,28 @@
 // Components/HorizontalCardsRowBanner.js
-import styles from "@/Styles/Components/landing/main_banner.module.css";
+import styles from "@/Styles/Components/landing/main_banner.module.scss";
 import Image from "next/image";
 import Constants from "../../Constants";
 const MainBanner = () => {
     return (
-        <div className="section section--zero-padding">
-            <section className={styles.banner}>
-                <div className={styles.overlay}></div>
-                <div className={styles.content}>
-                    <Image
-                        className={styles.titleLogo}
-                        width={100}
-                        height={100}
-                        src={Constants.images.logo}
-                        alt="Muzeum Chalupeni Logo"
-                        layout="responsive"
-                    />
-                    <h1 className={styles.title}>Muzeum Chalupění</h1>
-                    <div className={styles.subtitle}>
-                        <p className={styles.subtitleItem}>#Zemědělství </p>
-                        <p className={styles.subtitleItem}>#Historické expozice </p>
-                        <p className={styles.subtitleItem}>#Řemesla </p>
-                        <p className={styles.subtitleItem}>#Hračky </p>
-                    </div>
-                    <button className={styles.ctaButton}>Rezervace prohlídek</button>
+        <div className={styles.banner}>
+            <div className={styles.overlay}></div>
+            <div className={styles.content}>
+                <Image
+                    className={styles.titleLogo}
+                    width={300}
+                    height={300}
+                    src={Constants.images.logo}
+                    alt="Muzeum Chalupeni Logo"
+                />
+                <h1 className={styles.title}>Muzeum Chalupění</h1>
+                <div className={styles.subtitle}>
+                    <p className={styles.subtitleItem}>#Zemědělství </p>
+                    <p className={styles.subtitleItem}>#Historické expozice </p>
+                    <p className={styles.subtitleItem}>#Řemesla </p>
+                    <p className={styles.subtitleItem}>#Hračky </p>
                 </div>
-            </section>
+                <button className={styles.ctaButton}>Rezervace prohlídek</button>
+            </div>
         </div>
     );
 };
